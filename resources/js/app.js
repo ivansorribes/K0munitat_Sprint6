cc53677176f84e28f4f768314e8ff2571dc7762c
JordiNavGom <jordinavarro@iesmontsia.org>
2024-02-01 16:20:35 +0100

    conflicts

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,13 +3,10 @@ import './bootstrap';
 
 import './Codea.jsx'
 
-<<<<<<< Updated upstream
 import './login.jsx'
-=======
 import './login.jsx'
 
 import './components/Map.jsx'
 
 import './components/Modal.jsx'
 import './components/Slider.jsx'
->>>>>>> Stashed changes
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,8 @@ module.exports = {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
-<<<<<<< Updated upstream
     "./resources/js/*.jsx",
 
-=======
-    "./resources/**/*.jsx",
->>>>>>> Stashed changes
   ],
   theme: {
     extend: {},
