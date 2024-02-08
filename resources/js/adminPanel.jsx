929310f77b6f8240f03c6c7522aab7947bbf6b45
Ivan Sorribes <ivansorribes@iesmontsia.org>
2024-02-08 16:33:17 +0100

    panel admin creado

--- resources/js/adminPanel.jsx
+++ resources/js/adminPanel.jsx
@@ -8,6 +8,29 @@ import { useEffect } from 'react'; // Agrega useEffect a la importación
 
 export default function AdminPanel() {
     useEffect(() => {
+
+        const sideMenu = document.querySelector('aside');
+        const menuBtn = document.getElementById('menu-btn');
+        const closeBtn = document.getElementById('close-btn')
+
+        const darkMode = document.querySelector('.dark-mode')
+
+        menuBtn.addEventListener('click', () => {
+            sideMenu.style.display = 'block'
+        });
+
+        closeBtn.addEventListener('click', () => {
+            sideMenu.style.display = 'none'
+        });
+
+        darkMode.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode-variables');
+            darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+            darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+
+        })
+
+
         const Orders = [
             {
                 productName: 'JavasCript Tutorial',
@@ -19,7 +42,7 @@ export default function AdminPanel() {
                 productName: 'Css full course',
                 productNumber: '97245',
                 paymentStatus: 'Refunded',
-                status: 'Declined'   
+                status: 'Declined'
             },
             {
                 productName: 'Flex-box tutorial',
@@ -47,13 +70,12 @@ export default function AdminPanel() {
             <aside>
                 <div className="toggle">
                     <div className="logo">
-                        <img src="img/Logo_K0munitat-removebg-preview.png"></img>
                         <h2>K0<span className="danger">munitat</span></h2>
                     </div>
                     <div className="close" id="close-btn">
                         <span className="material-icons-sharp">
                             close
-                        </span>                        
+                        </span>
                     </div>
                 </div>
 
@@ -74,32 +96,19 @@ export default function AdminPanel() {
                         <span className="material-icons-sharp">
                             receipt_long
                         </span>
-                        <h3>History</h3>
+                        <h3>Posts</h3>
                     </a>
                     <a href="#" className="active">
                         <span className="material-icons-sharp">
-                            insights
+                            group
                         </span>
-                        <h3>Analytics</h3>
+                        <h3>Communities</h3>
                     </a>
                     <a href="#">
                         <span className="material-icons-sharp">
                             mail_outline
                         </span>
-                        <h3>Tickets</h3>
-                        <span className="message-count">27</span>
-                    </a>
-                    <a href="#">
-                        <span className="material-icons-sharp">
-                            inventory
-                        </span>
-                        <h3>Sale List</h3>
-                    </a>
-                    <a href="#">
-                        <span className="material-icons-sharp">
-                            report_gmailerrorred
-                        </span>
-                        <h3>Reports</h3>
+                        <h3>Blog</h3>
                     </a>
                     <a href="#">
                         <span className="material-icons-sharp">
@@ -118,142 +127,142 @@ export default function AdminPanel() {
                             logout
                         </span>
                         <h3>Logout</h3>
-                    </a>    
+                    </a>
                 </div>
             </aside>
 
-        <main>
-            <h1>Analyticis</h1>
-            <div className="analyse">
-                <div className="sales">
-                    <div className="status">
-                        <div className="info">
-                            <h3>Total Sales</h3>
-                            <h1>$65.024</h1>
+            <main>
+                <h1>Analyticis</h1>
+                <div className="analyse">
+                    <div className="sales">
+                        <div className="status">
+                            <div className="info">
+                                <h3>Total Sales</h3>
+                                <h1>$65.024</h1>
+                            </div>
+                            <div className="progresss">
+                                <svg>
+                                    <circle cx="38" cy="38"
+                                        r="36"></circle>
+                                </svg>
+                                <div className="percentage">
+                                    <p>+81%</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="progresss">
-                            <svg>
-                                <circle cx="38" cy="38"
-                                r="36"></circle> 
-                            </svg>
-                            <div className="percentage">
-                                <p>+81%</p>
+                    </div>
+                    <div className="visits">
+                        <div className="status">
+                            <div className="info">
+                                <h3>Site visit</h3>
+                                <h1>24,981</h1>
+                            </div>
+                            <div className="progresss">
+                                <svg>
+                                    <circle cx="38" cy="38"
+                                        r="36"></circle>
+                                </svg>
+                                <div className="percentage">
+                                    <p>-48%</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="searches">
+                        <div className="status">
+                            <div className="info">
+                                <h3>Searches</h3>
+                                <h1>14,147</h1>
+                            </div>
+                            <div className="progresss">
+                                <svg>
+                                    <circle cx="38" cy="38"
+                                        r="36"></circle>
+                                </svg>
+                                <div className="percentage">
+                                    <p>+21%</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="visits">
-                    <div className="status">
+                <div className="new-users">
+                    <h2>New Users</h2>
+                    <div className="user-list">
+                        <div className="user">
+                            <img src="profile/images/snoopy17.jpg"></img>
+                            <h2>Tatiana</h2>
+                            <p>54 min ago</p>
+                        </div>
+                        <div className="user">
+                            <img src="profile/images/snoopy17.jpg"></img>
+                            <h2>Ivan</h2>
+                            <p>3 Hours Ago</p>
+                        </div>
+                        <div className="user">
+                            <img src="profile/images/snoopy17.jpg"></img>
+                            <h2>Cristian</h2>
+                            <p>6 Hours Ago</p>
+                        </div>
+                        <div className="user">
+                            <img src="img/plus_icon.png"></img>
+                            <h2>More</h2>
+                            <p>New User</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="recent-orders">
+                    <h2>Recent Orders</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Course Name</th>
+                                <th>Course Number</th>
+                                <th>Payment</th>
+                                <th>Status</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                    <a href="#">Show All</a>
+                </div>
+
+
+            </main>
+
+            <div className="right-section">
+                <div className="nav">
+                    <button id="menu-btn">
+                        <span className="material-icons-sharp">
+                            menu
+                        </span>
+                    </button>
+                    <div className="dark-mode">
+                        <span className="material-icons-sharp active">
+                            light_mode
+                        </span>
+                        <span className="material-icons-sharp">
+                            dark_mode
+                        </span>
+                    </div>
+
+                    <div className="profile">
                         <div className="info">
-                            <h3>Site visit</h3>
-                            <h1>24,981</h1>
+                            <p>Hey, <b>Reza</b></p>
+                            <small className="text-muted">Admin</small>
                         </div>
-                        <div className="progresss">
-                            <svg>
-                                <circle cx="38" cy="38"
-                                r="36"></circle> 
-                            </svg>
-                            <div className="percentage">
-                                <p>-48%</p>
-                            </div>
+                        <div className="profile-photo">
+                            <img src="profile/images/snoopy17.jpg" />
                         </div>
                     </div>
-                </div>
-                <div className="searches">
-                    <div className="status">
-                        <div className="info">
-                            <h3>Searches</h3>
-                            <h1>14,147</h1>
-                        </div>
-                        <div className="progresss">
-                            <svg>
-                                <circle cx="38" cy="38"
-                                r="36"></circle> 
-                            </svg>
-                            <div className="percentage">
-                                <p>+21%</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="new-users">
-                <h2>New Users</h2>
-                <div className="user-list">
-                    <div className="user">
-                        <img src="profile/images/snoopy17.jpg"></img>
-                        <h2>Tatiana</h2>
-                        <p>54 min ago</p>
-                    </div>
-                    <div className="user">
-                        <img src="profile/images/snoopy17.jpg"></img>
-                        <h2>Ivan</h2>
-                        <p>3 Hours Ago</p>
-                    </div>
-                    <div className="user">
-                        <img src="profile/images/snoopy17.jpg"></img>
-                        <h2>Cristian</h2>
-                        <p>6 Hours Ago</p>
-                    </div>
-                    <div className="user">
-                        <img src="profile/images/snoopy17.jpg"></img>
-                        <h2>More</h2>
-                        <p>New User</p>
-                    </div>
-                </div>
-            </div>
 
-            <div className="recent-orders">
-                <h2>Recent Orders</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Course Name</th>
-                            <th>Course Number</th>
-                            <th>Payment</th>
-                            <th>Status</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody></tbody>
-                </table>
-                <a href="#">Show All</a>
-            </div>
-
-
-        </main>
-
-        <div className="right-section">
-            <div className="nav">
-                <button id="menu-btn">
-                    <span className="material-icons-sharp">
-                        menu
-                    </span>
-                </button>
-                <div className="dark-mode">
-                    <span className="material-icons-sharp active">
-                        light_mode
-                    </span>
-                    <span className="material-icons-sharp">
-                        dark_mode
-                    </span>
                 </div>
 
-                <div className="profile">
-                    <div className="info">
-                        <p>Hey, <b>Reza</b></p>
-                        <small className="text-muted">Admin</small>
-                    </div>
-                    <div className="profile-photo">
-                        <img src="profile/images/snoopy17.jpg"/>
-                    </div>
-                </div>
-
-            </div>
-            
                 <div className="user-profile">
                     <div className="logo">
-                        <img src="img/Logo_K0munitat-removebg-preview.png"/>
+                        <img src="img/Logo_K0munitat_Pequeño.png" />
                         <h2>K0munitat</h2>
                         <p>Create your blog</p>
                     </div>
@@ -268,7 +277,7 @@ export default function AdminPanel() {
                     <div className="notification">
                         <div className="icon">
                             <span className="material-icons-sharp">
-                            volume_up
+                                volume_up
                             </span>
                         </div>
                         <div className="content">
@@ -302,7 +311,7 @@ export default function AdminPanel() {
                             </span>
                         </div>
                     </div>
-                    
+
                     <div className="notification add-reminder">
                         <div>
                             <span className="material-icons-sharp">
@@ -312,8 +321,8 @@ export default function AdminPanel() {
                         </div>
                     </div>
 
-                </div>  
-        </div>
+                </div>
+            </div>
 
 
         </div>
