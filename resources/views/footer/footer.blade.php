5e1215b6ef586c610d02599351a6b953915ddbc5
Macarena Gonzales <144931898+macarenagonzales@users.noreply.github.com>
2024-02-08 20:30:03 +0100

    footer

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -10,12 +10,12 @@
 </head>
 <body>
 </body>
-    <footer class="bg-[#fffdf9] dark:bg-gray-900 mx-auto sm:px-6 lg:px-10 shadow">
-        <div class="container mx-auto w-full max-w-screen-xl px-6 py-14 lg:py-8 border border-gray-200 dark:border-gray-700">
+    <footer class="bg-[#fffdf9] dark:bg-gray-900 mx-auto max-w-full  shadow">
+        <div class="container mx-auto w-full max-w-full px-6 py-14 lg:py-8 border border-gray-200 dark:border-gray-700">
             <div class="md:flex md:justify-between">
               <div class="mb-6 md:mb-0">
-                  <a href="{{ route('inicia-sesion') }}" class="flex items-center">
-                      <img src="{{ asset('img/Logo_K0munitat-removebg-preview.png') }}" class="hover:scale-110 h-8 me-3" alt="Logo Komunitat" />
+                  <a href="{{ route('about-us') }}" class="flex items-center">
+                      <img src="{{ asset('img/logo.png') }}" class="hover:scale-110 h-8 me-3" alt="Logo Komunitat" />
                       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
                   </a>
               </div>
@@ -24,10 +24,13 @@
                       <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                       <ul class="text-gray-500 dark:text-gray-400 font-medium">
                           <li class="mb-4">
-                              <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                              <a href="https://flowbite.com/" class="hover:underline">Home</a>
                           </li>
-                          <li>
-                              <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                          <li class="mb-4">
+                              <a href="https://tailwindcss.com/" class="hover:underline">Blog</a>
+                          </li>
+                          <li class="mb-4">
+                            <a href="https://tailwindcss.com/" class="hover:underline">Communities</a>
                           </li>
                       </ul>
                   </div>
@@ -57,7 +60,7 @@
           </div>
           <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div class="sm:flex sm:items-center sm:justify-between">
-              <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+              <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">Komunitat™</a>. All Rights Reserved.
               </span>
               <div class="flex mt-4 sm:justify-center sm:mt-0">
                   <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
