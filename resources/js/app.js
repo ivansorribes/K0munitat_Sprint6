fdcb08f0d352ff8f454ba52f2af3a78acad71859
JordiNavGom <jordinavarro@iesmontsia.org>
2024-02-08 18:50:01 +0100

    ultim fix antes del merge

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,9 @@
 import './bootstrap';
 
-import './login.jsx'
 
-import './components/Exchanges.jsx'
+import './components/Exchanges.jsx';
 import './PersonalProfile.jsx';
-import './AboutUs.jsx'
-import './components/Map.jsx'
+import './AboutUs.jsx';
+import './components/Map.jsx';
 import './AdvertisementList.jsx';
 import './AdminPanel.jsx';
