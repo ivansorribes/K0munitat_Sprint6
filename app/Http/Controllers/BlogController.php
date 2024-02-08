ec6defab8d7ad67e7cdf50d5b219b84041aa9034
Cristian Cutar <ccutar76@gmail.com>
2024-02-08 16:35:47 +0100

    BlogUpdates

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Blog::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            // Add validation for other fields as needed
+        ]);
+
+        Blog::create($request->all());
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $post = Blog::find($id);
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $post = Blog::find($id);
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            // Add validation for other fields as needed
+        ]);
+
+        $post = Blog::find($id);
+        $post->update($request->all());
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $post = Blog::find($id);
+        $post->delete();
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post deleted successfully');
+    }
+}
