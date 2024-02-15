338c7d0f02e97abd7753725ce67fdfdb260db169
Macarena Gonzales <144931898+macarenagonzales@users.noreply.github.com>
2024-02-15 18:58:59 +0100

    perfil asociado

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -41,7 +41,7 @@
                                 <path d="M10 0a6.671 6.671 0 00-6.667 6.667c0 3.817 3.115 6.667 6.667 6.667S16.667 10.484 16.667 6.667A6.671 6.671 0 0010 0zm0 10a3.333 3.333 0 010-6.667A3.333 3.333 0 0110 10zm7.887 7.887c-1.666-2.5-4.167-4.167-7.887-4.167s-6.221 1.666-7.887 4.167c-.276.415-.413.888-.413 1.416 0 .553.207 1.035.62 1.448.414.414.896.62 1.448.62.476 0 .96-.207 1.448-.62 1.084-1.084 2.5-1.667 4.246-1.667 1.747 0 3.163.583 4.246 1.667.487.414.972.62 1.448.62.476 0 .96-.207 1.448-.62.414-.414.62-.896.62-1.448 0-.528-.138-1.001-.413-1.416z"/>
                             </svg>
                             <div id="user-menu" style="z-index: 15" class="hidden absolute top-full right-0  mt-2 bg-white border rounded-md shadow-lg">
-                                <a href="{{ route('about-us') }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Perfil</a>
+                                <a href="{{ route('ProfileView') }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Perfil</a>
                                 <a href="{{ route('logout') }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Cerrar sesión</a>
                             </div>
                         </div>
