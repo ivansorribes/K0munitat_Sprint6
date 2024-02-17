a05947ae0df7ac4eeef7b9f31d91a852d5482d4e
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-02-17 12:47:09 +0100

    added seeder w/images

--- database/seeders/imagePostSeeder.php
+++ database/seeders/imagePostSeeder.php
@@ -22,24 +22,24 @@ class imagePostSeeder extends Seeder
                 'name' => "25lQqNX1SMes2GzLEQXoFlWRZikXfvNVNPzeohcz.webp",
                 'front_page' => 0,
             ],
-            // [
-            //     //id 2
-            //     'id_post' => 2,
-            //     'name' => 1,
-            //     'front_page' => 0,
-            // ],
-            // [
-            //     //id 3
-            //     'id_post' => 3,
-            //     'name' => 1,
-            //     'front_page' => 0,
-            // ],
-            // [
-            //     //id 4
-            //     'id_post' => 4,
-            //     'name' => 1,
-            //     'front_page' => 0,
-            // ],
+            [
+                //id 2
+                'id_post' => 2,
+                'name' => "MUiBq6jX1wrRsiqV2uj3sSOlnyZuGo0SdO3BbNM9.webp",
+                'front_page' => 0,
+            ],
+            [
+                //id 3
+                'id_post' => 3,
+                'name' => "Hdio2EgyFZJ1ewPTqXILEcVfNoQmwdbjzof3CJi4.webp",
+                'front_page' => 0,
+            ],
+            [
+                //id 4
+                'id_post' => 4,
+                'name' => "BCzwBOnM2ummVb2sx3G9t0n8RBGdY1YZrWxH6D86.webp",
+                'front_page' => 0,
+            ],
             // Añade más registros según sea necesario
         ];
 
