a0ac5dfb6eace791beab09d9765bd1d3ea9ad9a1
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-02-17 12:31:16 +0100

    add seeder w/images

--- database/seeders/imagePostSeeder.php
+++ database/seeders/imagePostSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\imagePost;
+use Illuminate\Support\Facades\DB;
+
 
 class imagePostSeeder extends Seeder
 {
@@ -13,7 +15,34 @@ class imagePostSeeder extends Seeder
      */
     public function run(): void
     {
-        //imagePost::factory(10)->create();
+        $imagesPosts = [
+            [
+                //id 1
+                'id_post' => 1,
+                'name' => "25lQqNX1SMes2GzLEQXoFlWRZikXfvNVNPzeohcz.webp",
+                'front_page' => 0,
+            ],
+            // [
+            //     //id 2
+            //     'id_post' => 2,
+            //     'name' => 1,
+            //     'front_page' => 0,
+            // ],
+            // [
+            //     //id 3
+            //     'id_post' => 3,
+            //     'name' => 1,
+            //     'front_page' => 0,
+            // ],
+            // [
+            //     //id 4
+            //     'id_post' => 4,
+            //     'name' => 1,
+            //     'front_page' => 0,
+            // ],
+            // Añade más registros según sea necesario
+        ];
 
+        DB::table('imagePost')->insert($imagesPosts);
     }
 }
--- database/seeders/postsSeeder.php
+++ database/seeders/postsSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\posts;
+use Illuminate\Support\Facades\DB;
 
 class postsSeeder extends Seeder
 {
@@ -13,8 +14,54 @@ class postsSeeder extends Seeder
      */
     public function run(): void
     {
-        //posts::factory(10)->create();
+        $posts = [
+            [
+                //id 1
+                'id_user' => 1,
+                'id_category' => 1,
+                'id_community' => 1,
+                'title' => "Título del post 1",
+                'description' => "Descripción del post 1",
+                'isActive' => 1,
+                'private' => 0,
+                'type' => 'post',
+            ],
+            [
+                //id 2
+                'id_user' => 1,
+                'id_category' => 1,
+                'id_community' => 1,
+                'title' => "Título del post 2",
+                'description' => "Descripción del post 2",
+                'isActive' => 1,
+                'private' => 0,
+                'type' => 'post',
+            ],
+            [
+                //id 3
+                'id_user' => 1,
+                'id_category' => 1,
+                'id_community' => 1,
+                'title' => "Título del advertisement 2",
+                'description' => "Descripción del advertisement 1",
+                'isActive' => 1,
+                'private' => 0,
+                'type' => 'advertisement',
+            ],
+            [
+                //id 4
+                'id_user' => 1,
+                'id_category' => 1,
+                'id_community' => 1,
+                'title' => "Título del advertisement 2",
+                'description' => "Descripción del advertisement 2",
+                'isActive' => 1,
+                'private' => 0,
+                'type' => 'advertisement',
+            ],
+            // Añade más registros según sea necesario
+        ];
 
-
+        DB::table('posts')->insert($posts);
     }
 }
