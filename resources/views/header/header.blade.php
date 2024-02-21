f1d41a6a9487996a004f0a7a05b2fb7da8a1988e
Macarena Gonzales <144931898+macarenagonzales@users.noreply.github.com>
2024-02-21 14:04:12 +0100

    modificacion botones login para usuarios no autenticados

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -74,24 +74,27 @@
             <a href="{{ route('home') }}">
                 <img src="{{ asset('img/logo.png') }}" alt="Logo Komunitat" class="hover:scale-110 h-10 mt-2">
             </a>
-            <!--Menu de Navegacion-->
-            <div class="space-y-1 space-x-8 ml-8 pb-3 border-t pt-2 hidden md:flex">
-                <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-[#fffdf9] w-full md:w-auto md:py-0 py-4 md:pl-0 pl-7 left-0">
-                    <li class="mx-4 my-6 md:my-0">
-                        <a href="{{ route('home') }}" class="block px-3 py-2 text-[#f4971e] rounded-md">Home</a>
-                    </li>
-                    <li class="mx-4 my-6 md:my-0">
-                        <a href="{{ route('blog') }}" class="block px-3 py-2 text-[#3d3c3b] transition-colors hover:text-[#f4971e] duration-500">Blog</a>
-                    </li>
-                    <li class="mx-4 my-6 md:my-0">
-                        <a href="{{ route('about-us') }}" class="block px-3 py-2 text-[#3d3c3b] transition-colors hover:text-[#f4971e] duration-500">About Us</a>
-                    </li>
-                    <div class="lg:flex items-center gap-6 space-x-4 md:flex items-center md:z-auto md:static absolute w-auto ml-8">
-                        <a href="{{ route('LoginView') }}" class="bg-[#155b2a] text-[#fffdf9] px-5 py-2 btn-action rounded-full hover:bg-[#f4971e] transition duration-300">Login</a>
-                        <a href="{{ route('RegisterView') }}" class="bg-gradient-to-br from-[#155b2a] to-[#adce71] text-[#fffdf9] px-5 py-2 btn-action rounded-full hover:bg-[#f4971e] transition duration-300">Sign up</a>
-                    </div>
-                </ul>
-            </div>
+           
+           
+        </div>
+         <!--Menu de Navegacion-->
+         <div class="space-y-1 space-x-8 ml-8 pb-3 border-t pt-2 hidden md:flex">
+            <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-[#fffdf9] w-full md:w-auto md:py-0 py-4 md:pl-0 pl-7 left-0">
+                <li class="mx-4 my-6 md:my-0">
+                    <a href="{{ route('home') }}" class="block px-3 py-2 text-[#f4971e] rounded-md">Home</a>
+                </li>
+                <li class="mx-4 my-6 md:my-0">
+                    <a href="{{ route('blog') }}" class="block px-3 py-2 text-[#3d3c3b] transition-colors hover:text-[#f4971e] duration-500">Blog</a>
+                </li>
+                <li class="mx-4 my-6 md:my-0">
+                    <a href="{{ route('about-us') }}" class="block px-3 py-2 text-[#3d3c3b] transition-colors hover:text-[#f4971e] duration-500">About Us</a>
+                </li>
+               
+            </ul>
+        </div>
+        <div class=" hidden lg:flex items-center gap-6 space-x-4 md:flex items-center md:z-auto md:static absolute w-auto ml-8">
+            <a href="{{ route('LoginView') }}" class="bg-[#155b2a] text-[#fffdf9] px-5 py-2 btn-action rounded-full hover:bg-[#f4971e] transition duration-300">Login</a>
+            <a href="{{ route('RegisterView') }}" class="bg-gradient-to-br from-[#155b2a] to-[#adce71] text-[#fffdf9] px-5 py-2 btn-action rounded-full hover:bg-[#f4971e] transition duration-300">Sign up</a>
         </div>
         <div>
             <button id="hamburger-button" class="text-3xl md:hidden coursor-pointer">
