459ed89d6ea2b250ac6d823c15fbecebb022737a
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-03-07 16:31:33 +0100

    little refactor
    - remove 'localhost' fetch
    - import script for advertisement form validations

--- resources/views/advertisements-posts/form-create-advertisement-post.blade.php
+++ resources/views/advertisements-posts/form-create-advertisement-post.blade.php
@@ -16,49 +16,7 @@
 @include('header.header')
 
 <body>
-
-    <script>
-        $(document).ready(function() {
-            $("#yourFormId").submit(function(event) {
-                var isValid = true;
-                var errors = [];
-                var $errorMessages = $("#errorMessages");
-                $errorMessages.hide();
-                $("#errorList").empty();
-                if (!$('input[name="type"]:checked').length) {
-                    errors.push("<li>Please select a type.</li>");
-                    isValid = false;
-                }
-                var title = $("#title").val().trim();
-                if (title === "") {
-                    errors.push("<li>Title is required.</li>");
-                    isValid = false;
-                }
-                var description = $("#description").val().trim();
-                if (description === "") {
-                    errors.push("<li>Description is required.</li>");
-                    isValid = false;
-                }
-                var categoryId = $('select[name="category_id"]').val();
-                if (categoryId === "") {
-                    errors.push("<li>Please select a category.</li>");
-                    isValid = false;
-                }
-                if (!$('#image')[0].files.length) {
-                    errors.push("<li>Please upload an image.</li>");
-                    isValid = false;
-                }
-                if (!isValid) {
-                    $("#errorList").html(errors.join(""));
-                    $errorMessages.show();
-                    event.preventDefault();
-                }
-            });
-            $(document).on('click', '#errorMessages svg', function() {
-                $("#errorMessages").hide();
-            });
-        });
-    </script>
+    <script src="{{ asset('js/FormAdvertisementValidation.js') }}"></script>
     <div class="flex justify-center items-center bg-gray-100 p-6 min-h-screen">
         <div class="relative py-3 sm:max-w-xl mx-auto">
             <div class="absolute inset-0 bg-gradient-to-r from-yellow-300 to-yellow-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
