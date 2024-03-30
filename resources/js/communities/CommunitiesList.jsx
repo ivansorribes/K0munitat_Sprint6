4a4ade41573478609f1c888822c8a2e944c3c5e9
michipce <michipce@gmail.com>
2024-03-30 20:13:58 +0100

    search input

--- resources/js/communities/CommunitiesList.jsx
+++ resources/js/communities/CommunitiesList.jsx
@@ -9,32 +9,29 @@ import ToggleButton from '../components/buttons/toggle';
 import { animateScroll as scroll } from 'react-scroll';
 import {ButtonCreate} from '../components/buttons';
 
-
 const CommunitiesList = () => {
   const [option, setOption] = useState('option1');
-  const [currentPage, setCurrentPage] = useState(1); // Empezamos desde la página 1
+  const [currentPage, setCurrentPage] = useState(1);
   const perPage = 9;
   const [communities, setCommunities] = useState([]);
   const [loading, setLoading] = useState(true);
   const scrollRef = useRef();
-  const [userCommunities, setUserCommunities] = useState([]); // Lista de comunidades del usuario
+  const [userCommunities, setUserCommunities] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(''); // Estado para almacenar el término de búsqueda
 
   useEffect(() => {
-    // Aquí debes realizar la llamada a tu API para obtener la lista de comunidades del usuario
-    // Reemplaza el contenido de este useEffect con tu lógica de obtención de datos
     const fetchUserCommunities = async () => {
       try {
         const response = await axios.get('/communitiesUserId');
-        setUserCommunities(response.data); // Actualiza el estado con las comunidades del usuario
+        setUserCommunities(response.data);
       } catch (error) {
         console.error('Error fetching user communities:', error);
       }
     };
 
     fetchUserCommunities();
-  }, []); // Esta llamada solo se realiza una vez al montar el componente
+  }, []);
 
-  // Usa el hook useApiSwitcher con el valor de la opción actual
   const { data: apiData, loading: apiLoading } = useApiSwitcher(option, currentPage);
 
   useEffect(() => {
@@ -45,7 +42,7 @@ const CommunitiesList = () => {
   useEffect(() => {
     const handleScroll = () => {
       if (scrollRef.current && scrollRef.current.scrollHeight - scrollRef.current.scrollTop === scrollRef.current.clientHeight) {
-        fetchData(); // Llama a fetchData() cuando se hace scroll al final
+        fetchData();
       }
     };
 
@@ -59,17 +56,15 @@ const CommunitiesList = () => {
   }, []);
 
   const fetchData = () => {
-    // Aquí debes implementar la lógica para cargar más datos cuando se hace scroll
-    // Por ejemplo, puedes aumentar el número de la página actual y hacer otra llamada a la API
     setCurrentPage(prevPage => prevPage + 1);
   };
 
   const toggleOption = () => {
     setOption(option === 'option1' ? 'option2' : 'option1');
-    setCurrentPage(1); // Reiniciamos la página cuando cambiamos la opción
-    setCommunities([]); // Limpiamos las comunidades cuando cambiamos la opción
-    fetchData(); // Volvemos a cargar los datos con la nueva opción
-    scrollToTop(); // Desplazamos al principio de la lista
+    setCurrentPage(1);
+    setCommunities([]);
+    fetchData();
+    scrollToTop();
   };
 
   const scrollToTop = () => {
@@ -80,32 +75,41 @@ const CommunitiesList = () => {
     });
   };
 
+  // Función para manejar el cambio en el cuadro de búsqueda
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filtrar comunidades basadas en el término de búsqueda
+  const filteredCommunities = communities.filter(community =>
+    community.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="container mx-auto mt-[5vw] md:mt-[8] lg:mt-[10] xl:mt-[12]">
       <div className="flex justify-between items-center mb-4">
         <ToggleButton onToggle={toggleOption} checked={option === 'option2'} text={option === 'option1' ? 'All Communities' : 'My Communities'} />
+        {/* Cuadro de búsqueda */}
+        <input
+          type="text"
+          placeholder="Search Communities"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+        />
         <a href="/communities/create" rel="noopener noreferrer">
           <ButtonCreate label="Create" />
         </a>
       </div>
       <div id="scroll-container" ref={scrollRef} style={{ overflowY: 'scroll', height: '60vh' }}>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {communities.map((community) => {
-          let option = 'edit';
-          if (community.private === 0 || userCommunities.includes(community.id)) {
-            option = 'enter';
-          } else {
-            option = 'send';
-          }
-
-          return (
+          {filteredCommunities.map((community) => (
             <CommunityCard
               key={community.id}
               community={community}
-              option={option}
+              option={userCommunities.includes(community.id) || community.private === 0 ? 'enter' : 'send'}
             />
-          );
-        })}
+          ))}
         </div>
         {loading && <p>Loading...</p>}
       </div>
