a0479212040b55e981980b9e785ee58a13ea7251
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-04-05 16:37:42 +0200

    added seeder for replies -> comments

--- app/Models/ReplyComment.php
+++ app/Models/ReplyComment.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class ReplyComment extends Model
 {
     use HasFactory;
+
+    protected $table = 'reply_comments';
+
+    protected $fillable = [
+        'reply',
+        'id_user',
+        'id_comment'
+    ];
+
+    public function comment()
+    {
+        return $this->belongsTo(commentsPosts::class, 'id_comment');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
