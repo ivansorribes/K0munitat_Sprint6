5c33b892805cd38048deb1ba9acef2ece259629c
michipce <michipce@gmail.com>
2024-05-03 22:24:31 +0200

    limpieza final

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,28 @@
-import './bootstrap';
+import "./bootstrap";
 
-import './components/buttons.jsx';
-import './components/Exchanges.jsx';
-import './PersonalProfile.jsx';
-import './Contact.jsx';
-import './AboutUs.jsx';
-import './components/Map.jsx';
-import './advertisements-posts/AdvertisementList.jsx';
-import './AdminPanel.jsx';
-import './blog.jsx'
+import "./components/buttons.jsx";
+import "./components/Exchanges.jsx";
+import "./PersonalProfile.jsx";
+import "./Contact.jsx";
+import "./AboutUs.jsx";
+import "./components/Map.jsx";
+import "./advertisements-posts/AdvertisementList.jsx";
+import "./AdminPanel.jsx";
+import "./blog.jsx";
 
-import './communities/communitiesFormCreate.jsx';
-import './communities/communitiesFormEdit.jsx';
-import './communities/CommunityCard.jsx';
-import './communities/CommunitiesList.jsx';
-import './advertisements-posts/AdvertisementDetails.jsx';
-import './advertisements-posts/AdvertisementComments.jsx';
-import './Sidebar.jsx';
+import "./communities/communitiesFormCreate.jsx";
+import "./communities/CommunityCard.jsx";
+import "./communities/CommunitiesList.jsx";
+import "./advertisements-posts/AdvertisementDetails.jsx";
+import "./advertisements-posts/AdvertisementComments.jsx";
+import "./Sidebar.jsx";
 
-import './Taula.jsx';
-import './components/HeartButton.jsx';
-import './components/CommentLikeButton.jsx';
-import './components/ReplyBox.jsx';
+import "./Taula.jsx";
+import "./components/HeartButton.jsx";
+import "./components/CommentLikeButton.jsx";
+import "./components/ReplyBox.jsx";
 
-import './EditPersonalProfile.jsx';
-import './events/events.jsx';
-import './components/ReplyLikeButton.jsx'
-import './helpers/api.jsx'
-
-
-
+import "./EditPersonalProfile.jsx";
+import "./events/events.jsx";
+import "./components/ReplyLikeButton.jsx";
+import "./helpers/api.jsx";
